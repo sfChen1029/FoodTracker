681f83ac48a734c24c62a748e8e969bdbb8e0aee
Steven Chen <steven.fx.chen@gmail.com>
2019-05-25 13:29:46 -0700

    My initial project setup

---
+++ FoodTracker/FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Steven Chen on 5/25/19.
+//  Copyright © 2019 Steven Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
